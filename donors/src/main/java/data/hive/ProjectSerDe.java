6975c6f545b239f860e0f31e19baada4d9e52930
Nicolas DI TULLIO <n.ditullio@hotmail.fr>
2016-02-12 00:23:19 +0800

    Updated hive SerDe classes

--- donors/src/main/java/data/hive/ProjectSerDe.java
+++ donors/src/main/java/data/hive/ProjectSerDe.java
@@ -22,13 +22,13 @@ import org.apache.hadoop.hive.serde2.typeinfo.TypeInfoUtils;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Writable;
 
-import data.writable.DonationWritable;
+import data.writable.ProjectWritable;
 
-public class DonationSerDe extends AbstractSerDe {
+public class ProjectSerDe extends AbstractSerDe {
 
-	public static final Log LOG = LogFactory.getLog(DonationSerDe.class.getName());
+	public static final Log LOG = LogFactory.getLog(ProjectSerDe.class.getName());
 
-	private static final int NUM_COLUMNS = 17;
+	private static final int NUM_COLUMNS = 30;
 	private List<String> columnNames;
 	private StandardStructObjectInspector objInspector;
 	private ArrayList<Object> row;
@@ -36,29 +36,42 @@ public class DonationSerDe extends AbstractSerDe {
 	@Override
 	public Object deserialize(Writable obj) throws SerDeException {
 
-		if (!(obj instanceof DonationWritable)) {
+		if (!(obj instanceof ProjectWritable)) {
 			throw new SerDeException(String.format("Expected type %s but received %s",
-					DonationWritable.class.getName(), obj.getClass().getName()));
+					ProjectWritable.class.getName(), obj.getClass().getName()));
 		}
 
-		DonationWritable donation = (DonationWritable) obj;
-		row.set(0, donation.donation_id);
-		row.set(1, donation.project_id);
-		row.set(2, donation.donor_city);
-		row.set(3, donation.donor_state);
-		row.set(4, donation.donor_is_teacher);
-		row.set(5, donation.ddate);
-		row.set(6, donation.amount);
-		row.set(7, donation.support);
-		row.set(8, donation.total);
-		row.set(9, donation.payment_method);
-		row.set(10, donation.payment_inc_acct_credit);
-		row.set(11, donation.payment_inc_campaign_gift_card);
-		row.set(12, donation.payment_inc_web_gift_card);
-		row.set(13, donation.payment_promo_matched);
-		row.set(14, donation.via_giving_page);
-		row.set(15, donation.thank_you_packet);
-		row.set(16, donation.message);
+		ProjectWritable project = (ProjectWritable) obj;
+		row.set(0, project.project_id);
+		row.set(1, project.teacher_acctid);
+		row.set(2, project.school_id);
+		row.set(3, project.school_latitude);
+		row.set(4, project.school_longitude);
+		row.set(5, project.school_city);
+		row.set(6, project.school_state);
+		row.set(7, project.school_country);
+		row.set(8, project.teacher_prefix);
+		row.set(9, project.primary_focus_subject);
+		row.set(10, project.primary_focus_area);
+		row.set(11, project.secondary_focus_subject);
+		row.set(12, project.secondary_focus_area);
+		row.set(13, project.resource_type);
+		row.set(14, project.poverty_level);
+		row.set(15, project.grade_level);
+		row.set(16, project.vendor_shipping_charges);
+		row.set(17, project.sales_tax);
+		row.set(18, project.payment_processing_charges);
+		row.set(19, project.fulfillment_labor_materials);
+		row.set(20, project.total_price_excluding_opt_donation);
+		row.set(21, project.total_price_including_opt_donation);
+		row.set(22, project.students_reached);
+		row.set(23, project.total_donations);
+		row.set(24, project.num_donors);
+		row.set(25, project.funding_status);
+		row.set(26, project.date_posted);
+		row.set(27, project.date_completed);
+		row.set(28, project.date_thank_you_packet_mailed);
+		row.set(29, project.date_expiration);
 		return row;
 	}
 
@@ -82,11 +95,13 @@ public class DonationSerDe extends AbstractSerDe {
 
 		// Create inspectors for each attribute
 		List<ObjectInspector> inspectors = new ArrayList<ObjectInspector>(columnNames.size());
-		for (int i = 0; i <= 5; i++)
+		for (int i = 0; i <= 14; i++)
 			inspectors.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
-		for (int i = 6; i <= 8; i++)
+		for (int i = 15; i <= 21; i++)
 			inspectors.add(PrimitiveObjectInspectorFactory.javaFloatObjectInspector);
-		for (int i = 9; i <= 16; i++)
+		for (int i = 22; i <= 24; i++)
+			inspectors.add(PrimitiveObjectInspectorFactory.javaIntObjectInspector);
+		for (int i = 25; i <= 29; i++)
 			inspectors.add(PrimitiveObjectInspectorFactory.javaStringObjectInspector);
 
 		// StandardStruct uses ArrayList to store the row.
